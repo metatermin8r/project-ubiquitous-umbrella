9c451f1c421ae614eee75657b334f1092437110d
lydiabarkley <lydiabarkley72@gmail.com>
2023-12-19 09:33:35 -0600

    Added pause menu

--- UbiquitousUmbrella/Assets/Scripts/UI/PauseSystem.cs
+++ UbiquitousUmbrella/Assets/Scripts/UI/PauseSystem.cs
@@ -1,48 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseSystem : MonoBehaviour
-    { 
-        public static bool gameIsPaused;
-        public GameObject pauseMenu;
+{
+    public static bool gameIsPaused;
+    public GameObject pauseMenu;
+    public GameObject PlayerHud;
 
-    void Start ()
+
+    void Start()
     {
         pauseMenu.SetActive(false);
+        PlayerHud.SetActive(true);
     }
-        void Update()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                gameIsPaused = !gameIsPaused;
-                PauseGame();
-            }
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
 
-            else
-            {
+        else
+        {
             ResumeGame();
-            }
-        }
-        void PauseGame()
-        {
-            if (gameIsPaused)
-            {
-                Time.timeScale = 0f;
-                pauseMenu.SetActive(true);
-            }
-            else
-            {
-                Time.timeScale = 1;
-            }
-        }
-
-        void ResumeGame()
-        {
-            if (!gameIsPaused)
-            {
-                Time.timeScale = 1;
-                pauseMenu.SetActive(false);
-            }
         }
     }
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 1f;
+            pauseMenu.SetActive(true);
+            PlayerHud.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (!gameIsPaused)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            PlayerHud.SetActive(true);
+        }
+
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
