c66dc62a9d7b56fcad0964ef459d1359e6252941
lydiabarkley <lydiabarkley72@gmail.com>
2024-01-22 11:55:17 -0600

    added some random assets
    added grass, more rocks, and a light... not all of these things are necessarily useful.

--- UbiquitousUmbrella/Assets/Inventory/Scripts/ItemPickupable.cs
+++ UbiquitousUmbrella/Assets/Inventory/Scripts/ItemPickupable.cs
@@ -4,45 +4,45 @@ using UnityEngine;
 
 public class ItemPickupable : MonoBehaviour
 {
-    public GameObject itemObject;
-    public InventoryController inventoryController;
-    public GameObject pickUpUI;
-    public ItemDataHolder itemDataHolder;
+  //  public GameObject itemObject;
+  //  public InventoryController inventoryController;
+  //  public GameObject pickUpUI;
+  //  public ItemDataHolder itemDataHolder;
     // public GameObject Inventory;
     //public int selectedItemID;
 
     //reference to scriptable object
     //public ItemData itemData;
 
-    public int id;
+ //   public int id;
 
-    void Start()
-    {
-        pickUpUI.SetActive(false);
-        id = itemDataHolder.ItemNum;
-    }
+ //   void Start()
+ //   {
+ //       pickUpUI.SetActive(false);
+//        id = itemDataHolder.ItemNum;
+  //  }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            pickUpUI.SetActive(true);
-            if (Input.GetKey(KeyCode.E)) { 
+    //private void OnTriggerStay(Collider other)
+    //{
+      //  if (other.gameObject.tag == "Player")
+        //{
+          //  pickUpUI.SetActive(true);
+          //  if (Input.GetKey(KeyCode.E)) { 
                 //Inventory.SetActive(true);
-                inventoryController.CreateRandomItem();
-                Debug.Log("you just picked up item:" + id);
-                if (itemObject != null)
-                {
-                    this.itemObject.SetActive(false);
-                    pickUpUI.SetActive(false);
-                }
-                Debug.Log("Destroy object!");
-            }
-        }
-    }
+               // inventoryController.CreateRandomItem();
+               // Debug.Log("you just picked up item:" + id);
+               // if (itemObject != null)
+               // {
+                 //   this.itemObject.SetActive(false);
+                  //  pickUpUI.SetActive(false);
+             //   }
+          //      Debug.Log("Destroy object!");
+         //   }
+     //   }
+  //  }
 
-    void OnTriggerExit(Collider other)
-    {
-        pickUpUI.SetActive(false);
-    }
+  //  void OnTriggerExit(Collider other)
+ //   {
+  //      pickUpUI.SetActive(false);
+  //  }
 }
