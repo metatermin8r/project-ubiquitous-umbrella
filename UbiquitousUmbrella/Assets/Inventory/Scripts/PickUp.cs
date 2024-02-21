547060276c5df06efa164830911efbd17194541d
lydiabarkley <lydiabarkley72@gmail.com>
2024-02-20 22:21:12 -0600

    Revert "Revert "Revert "added weapon swap animation to HUD and  worked in inventory system"""
    This reverts commit b36da6b3cc047239facf5522123865cf5bf7a2fa.

--- UbiquitousUmbrella/Assets/Inventory/Scripts/PickUp.cs
+++ UbiquitousUmbrella/Assets/Inventory/Scripts/PickUp.cs
@@ -1,52 +1,25 @@
 using System.Collections;
-using System.ComponentModel.Design;
-using System.Reflection;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
     public HeldItem item = new HeldItem("Item Name", 1, 1);
     public GameObject pickUpUI;
-    public Transform InteractorSource; //this is a reference to the player 
-    public float InteractRange; //the distance from which you can pick up an object
 
-    private void Start()
+    void OnTriggerEnter(Collider other)
     {
-        pickUpUI.SetActive(false);
-    }
+        pickUpUI.SetActive(true);
 
-    private void Update()
-    {
-        Ray ray = new Ray(InteractorSource.position, InteractorSource.forward);
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, InteractRange))
+       if (Input.GetKey(KeyCode.E))
         {
-            pickUpUI.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
+            if (other.CompareTag("Player"))
             {
                 InventoryController.instance.AddNewItem(item);
                 Destroy(gameObject);
-                pickUpUI.SetActive(false);
             }
         }
-        else
-        {
-            pickUpUI.SetActive(false);
-        }
     }
-
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    pickUpUI.SetActive(true);
-
-    //   if (Input.GetKey(KeyCode.E))
-    //    {
-    //        if (other.CompareTag("Player"))
-    //        {
-    //            InventoryController.instance.AddNewItem(item);
-    //            Destroy(gameObject);
-    //        }
-    //    }
-    //}
 
     void OnTriggerExit(Collider other)
     {
